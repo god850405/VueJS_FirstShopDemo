8bd02b5b0785fabd51e33c2046ef2a7d69cef116
Zihcian Chen <35187114+god850405@users.noreply.github.com>
2022-06-29 10:13:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -30,7 +30,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: '/vue-shopping-cart-practice/',
 
         /**
          * Source Maps
